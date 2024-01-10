792e35d686233ce188a25b50900369c07ed3bee2
hawariyaw paulos <hawipaul308@gmail.com>
2024-01-10 12:36:10 -0800

    add two numbers linked list

---
+++ main.py
@@ -0,0 +1,31 @@
+# here in this question we are asked to add two numbers given in the linked list
+# they are in reverse order making it easy for us to add, with-out reversing it
+# [2,4,5] [6,3,2]      out-put = [8, 7, 7]
+Definition for singly-linked list.
+  class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode()
+        cur = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            sum = v1 + v2 + carry
+            carry = sum // 10
+            sum = sum % 10
+            cur.next = ListNode(sum)
+            cur = cur.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
